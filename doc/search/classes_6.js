60bb477f901516f9e29021a260be352c2c69b2f5
Yann-Ael Muller <ymullr@gmail.com>
2015-05-08 11:59:57 +0200

    Update doc

--- doc/search/classes_6.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['vecarray',['vecarray',['../structndata_1_1vecarray.html',1,'ndata']]],
-  ['vecarray_3c_20long_2c_20ndims_20_3e',['vecarray&lt; long, ndims &gt;',['../structndata_1_1vecarray.html',1,'ndata']]]
-];
